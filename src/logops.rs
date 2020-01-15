d5214c428be05b02eb961e5a76256f1764469d36
Eddie Jeselnik <eddie@jeselnik.xyz>
2020-01-16 00:39:45 +1100

    File to Track Vector Implemented

--- src/logops.rs
+++ src/logops.rs
@@ -1,34 +1,50 @@
 use std::fs;
-use std::result;
+use std::string;
 use std::vec;
 
 const AUDIOSCROBBLER_HEADER: &str = "#AUDIOSCROBBLER/";
-const TRACKS_BEGIN_INDEX: usize = 3;
 const LISTENED: &str = "\tL\t";
+const SEPARATOR: &str = "\t";
 
-struct Track {
-    artist: String,
-    album: String,
-    title: String,
-    timestamp: u64,
+/* Track data begins after this line in the log */
+const TRACKS_BEGIN_INDEX: usize = 3;
+/* Indexes used when iterating over track data */
+const ARTIST_INDEX: usize = 0;
+const ALBUM_INDEX: usize = 1;
+const TITLE_INDEX: usize = 2;
+const TIMESTAMP_INDEX: usize = 6;
+
+pub struct Track {
+    pub artist: String,
+    pub album: String,
+    pub title: String,
+    pub timestamp: u64,
 }
 
-pub fn log_to_vec(path: &str) -> Result<&str, &str> {
-    let file = fs::read_to_string(path)
-    .expect("Error Opening File");
+pub fn log_to_vec(path: &str) -> vec::Vec<Track> {
+    let file = fs::read_to_string(path).expect("Error Opening File");
 
     if !file.contains(AUDIOSCROBBLER_HEADER) {
-        return result::Result::Err("Not a valid .scrobbler.log!");
+        panic!("Not a valid .scrobbler.log!");
     } else {
-        //let mut tracks = vec::Vec::new();
+        let mut tracks = vec::Vec::new();
         let file_as_lines: Vec<&str> = file.lines().collect();
 
         for index in TRACKS_BEGIN_INDEX..file_as_lines.len() {
             if file_as_lines[index].contains(LISTENED) {
-                println!("{}", file_as_lines[index]);
+                let track_str: Vec<&str> = file_as_lines[index].split(SEPARATOR).collect();
+
+                let track = Track {
+                    artist: string::String::from(track_str[ARTIST_INDEX]),
+                    album: string::String::from(track_str[ALBUM_INDEX]),
+                    title: string::String::from(track_str[TITLE_INDEX]),
+                    timestamp: track_str[TIMESTAMP_INDEX].parse().unwrap(),
+                };
+
+                &tracks.push(track);
             }
         }
 
-        return result::Result::Ok("Yeah it's working'");
+        return tracks;
     }
 }
