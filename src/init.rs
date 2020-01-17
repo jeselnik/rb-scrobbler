192f94c443755ba33803b9c0c56d89a995c74682
Eddie Jeselnik <eddie@jeselnik.xyz>
2020-01-17 18:44:36 +1100

    Refactor & clean for my continued sanity

---
+++ src/init.rs
@@ -0,0 +1,50 @@
+extern crate clap;
+use clap::{App, Arg};
+
+pub fn app_info() -> clap::App<'static,'static> {
+    let app = App::new("Minimal Rockbox Scrobbler")
+        .version("0.1")
+        .author("Eddie Jeselnik <eddie@jeselnik.xyz>")
+        .about("Submit .scrobbler.log files to last.fm")
+        .arg(
+            Arg::with_name("file")
+                .short("f")
+                .long("file")
+                .value_name("FILE")
+                .help("File path to your .scrobbler.log")
+                .required(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("offset")
+                .short("o")
+                .long("offset")
+                .value_name("OFFSET")
+                .help("Offset (in hours) of local time to UTC")
+                .required(false)
+                .takes_value(true)
+                .default_value("0"),
+        )
+        .arg(
+            Arg::with_name("user")
+            .short("u")
+            .long("username")
+            .value_name("USERNAME")
+            .help("Your last.fm username (only required for 
+            initial authentication)")
+            .required(false)
+            .takes_value(true)
+            .default_value("")
+            )
+        .arg(
+            Arg::with_name("pass")
+            .short("p")
+            .long("password")
+            .value_name("PASSWORD (only required for 
+            initial authentication)")
+            .required(false)
+            .takes_value(true)
+            .default_value(""));
+
+    return app;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,29 @@
-use std::io;
+mod init;
 mod log;
 extern crate app_dirs;
 use app_dirs::*;
-extern crate clap;
-use clap::{App, Arg};
 extern crate rustfm_scrobble;
 
 fn main() {
-    let app = App::new("Minimal Rockbox Scrobbler")
-        .version("0.1")
-        .author("Eddie Jeselnik <eddie@jeselnik.xyz>")
-        .about("Submit .scrobbler.log files to last.fm")
-        .arg(
-            Arg::with_name("file")
-                .short("f")
-                .long("file")
-                .value_name("FILE")
-                .help("File path to your .scrobbler.log")
-                .required(true)
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("offset")
-                .short("o")
-                .long("offset")
-                .value_name("OFFSET")
-                .help("Offset (in hours) of local time to UTC")
-                .required(false)
-                .takes_value(true)
-                .default_value("0"),
-        )
-        .arg(
-            Arg::with_name("user")
-            .short("u")
-            .long("username")
-            .value_name("USERNAME")
-            .help("Your last.fm username (only required for 
-            initial authentication)")
-            .required(false)
-            .takes_value(true)
-            .default_value("")
-            )
-        .arg(
-            Arg::with_name("pass")
-            .short("p")
-            .long("password")
-            .value_name("PASSWORD (only required for 
-            initial authentication)")
-            .required(false)
-            .takes_value(true)
-            .default_value(""));
-
     const APP_INFO: app_dirs::AppInfo = app_dirs::AppInfo{name:"rb-scrobbler", author:"Eddie Jeselnik"};
-    let arguments = app.get_matches();
-
-    let username = arguments.value_of("user").unwrap();
-    let password = arguments.value_of("pass").unwrap();
-
     const API_KEY: &str = "INSERT_API_KEY";
     const API_SECRET: &str = "INSERT_API_SECRET";
+
+    let app = init::app_info();
+
+    let arguments = app.get_matches();
+    let username = arguments.value_of("user").unwrap();
+    let password = arguments.value_of("pass").unwrap();
+    let file_path = arguments.value_of("file").unwrap();
+    /* Get argument value and unwrap to type str, then parse the string and unwrap str for
+     * conversion to f32*/
+    let timezone_offset: f32 = arguments.value_of("offset").unwrap().parse().unwrap();
 
     if username != "" && password != "" {
         let mut scrobbler = rustfm_scrobble::Scrobbler::new(API_KEY, API_SECRET);
         let response = scrobbler.authenticate_with_password(username, password);
         println!("{:?}", response);
     }
-
-    let file_path = arguments.value_of("file").unwrap();
-    /* Get argument value and unwrap to type str, then parse the string and unwrap str for
-     * conversion to f32*/
-    let timezone_offset: f32 = arguments.value_of("offset").unwrap().parse().unwrap();
 
     let mut tracks = log::as_vec(file_path);
 
