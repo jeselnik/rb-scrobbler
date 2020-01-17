5b3fe625c2ba353478cf4375c4d910a0427af00b
Eddie Jeselnik <eddie@jeselnik.xyz>
2020-01-17 17:52:33 +1100

    New arguments for authentication

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,27 @@ fn main() {
                 .required(false)
                 .takes_value(true)
                 .default_value("0"),
-        );
+        )
+        .arg(
+            Arg::with_name("user")
+            .short("u")
+            .long("username")
+            .value_name("USERNAME")
+            .help("Your last.fm username (only required for 
+            initial authentication)")
+            .required(false)
+            .takes_value(true)
+            .default_value("")
+            )
+        .arg(
+            Arg::with_name("pass")
+            .short("p")
+            .long("password")
+            .value_name("PASSWORD (only required for 
+            initial authentication)")
+            .required(false)
+            .takes_value(true)
+            .default_value(""));
 
     let arguments = app.get_matches();
 
