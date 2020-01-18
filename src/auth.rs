71109cde2cfd632613d8bcec38a431ef22b0cd0e
Eddie Jeselnik <eddie@jeselnik.xyz>
2020-01-18 17:04:01 +1100

    Skeleton for initial authentication

--- src/auth.rs
+++ src/auth.rs
@@ -1,7 +1,23 @@
+use std::fs;
 extern crate app_dirs;
 use app_dirs::*;
+extern crate rustfm_scrobble;
+use rustfm_scrobble::Scrobbler;
 
-const APP_INFO: app_dirs::AppInfo = app_dirs::AppInfo {
+const APP_INFO: AppInfo = app_dirs::AppInfo {
     name: "rb-scrobbler",
     author: "Eddie Jeselnik",
 };
+
+pub fn initial_authentication(scrob: &mut Scrobbler, user: &str, pass: &str) {
+    let auth_res = scrob.authenticate_with_password(user, pass);
+    if auth_res.is_ok() {
+        println!("Authenticated!");
+        let session_key = scrob.session_key()
+            .expect("Couldn't get session key!");
+        fs::write("placeholder-path", session_key)
+            .expect("Couldn't save session key!'");
+    } else {
+        panic!("Authentication failed!");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod auth;
 mod init;
 mod log;
 extern crate rustfm_scrobble;
