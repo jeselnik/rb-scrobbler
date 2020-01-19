fe0cba5c046a097175266ec8a6e763ad8248db63
Eddie Jeselnik <eddie@jeselnik.xyz>
2020-01-19 16:34:42 +1100

    First implementation of authentication

--- src/auth.rs
+++ src/auth.rs
@@ -1,4 +1,6 @@
 use std::fs;
+extern crate dirs;
+use dirs::*;
 extern crate rustfm_scrobble;
 use rustfm_scrobble::Scrobbler;
 
@@ -7,8 +9,26 @@ pub fn initial_authentication(scrob: &mut Scrobbler, user: &str, pass: &str) {
     if auth_res.is_ok() {
         println!("Authenticated!");
 
+        let mut config_directory = config_dir()
+            .expect("Couldn't get config directory!");
+
+        config_directory.push("rb-scrobbler");
+
+        fs::create_dir(config_directory)
+            .expect("Couldn't create config directory!");
+
+        let mut key_directory = config_dir()
+            .expect("Couldn't get config directory!");
+
+        key_directory.push("rb-scrobbler");
+        key_directory.push("session");
+        key_directory.set_extension("key");
+
         let session_key = scrob.session_key()
             .expect("Couldn't get session key!");
+
+        fs::write(key_directory, session_key)
+            .expect("Couldn't save session key to directory!");
 
     } else {
         panic!("Authentication failed!");
