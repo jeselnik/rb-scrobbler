c3e360615966a188607a0211f4b6332cf2970f07
Edward Jeselnik <s3782519@student.rmit.edu.au>
2021-07-09 21:15:58 +1000

    Open log and check if file is valid

--- rb-scrobbler.go
+++ rb-scrobbler.go
@@ -1,18 +1,46 @@
 package main
 
 import (
+	"errors"
 	"flag"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 )
+
+const (
+	AUDIOSCROBBLER_HEADER = "#AUDIOSCROBBLER/"
+)
+
+func importLog(path *string) ([]string, error) {
+	logFile, err := os.Open(*path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logFile.Close()
+
+	logInBytes, err := ioutil.ReadAll(logFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	logAsLines := strings.Split(string(logInBytes), "\n")
+	if !strings.Contains(logAsLines[0], AUDIOSCROBBLER_HEADER) {
+		return logAsLines, errors.New("Not a valid .scrobbler.log!")
+	} else {
+		return logAsLines, nil
+	}
+}
 
 func main() {
 	logPath := flag.String("f", "", "Path to .scrobbler.log")
 	flag.Parse()
 
-	logFile, err := os.Open(*logPath)
+	scrobblerLog, err := importLog(logPath)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer logFile.Close()
+	fmt.Println(scrobblerLog[0])
 }
