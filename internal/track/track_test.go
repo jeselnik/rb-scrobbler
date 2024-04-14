74d1579179866cc97cce06dc318679777661ef7b
Eddie Jeselnik <eddie@jeselnik.xyz>
2024-04-14 11:27:03 +1000

    fix tests

--- internal/track/track_test.go
+++ internal/track/track_test.go
@@ -1,83 +1,75 @@
 package track
 
 import (
+	"strconv"
 	"testing"
+	"time"
 )
 
 const (
-	ZERO_OFFSET    = "0h"
+	ZERO_OFFSET    = 0
 	TEST_TIMESTAMP = "1579643462"
 )
 
-/*
-fix this later
 func TestLogLineToTrack(t *testing.T) {
 	testCases := []struct {
-		name           string
-		input          string
-		offset         string
-		expectedTrack  Track
-		expectedListen bool
+		name          string
+		input         []string
+		offset        float64
+		expectedTrack Track
 	}{
 		{"SkipTrack",
-			"50 Cent	Get Rich Or Die Tryin'	In Da Club	2" +
-				"	179	S	1579643462",
+			[]string{"50 Cent", "Get Rich Or Die Tryin'", "In Da Club", "2", "179", "S", "1579643462"},
 			ZERO_OFFSET,
 			Track{
 				artist:    "50 Cent",
 				album:     "Get Rich Or Die Tryin'",
 				title:     "In Da Club",
 				timestamp: "1579643462"},
-			false},
+		},
 		{"TimelessSupport",
-			"CHVRCHES	The Bones of What You Believe	The Mother We Share" +
-				"	1	120	L	0",
+			[]string{"CHVRCHES", "The Bones of What You Believe", "The Mother We Share", "1", "120", "L", "0"},
 			ZERO_OFFSET,
 			Track{
 				artist:    "CHVRCHES",
 				album:     "The Bones of What You Believe",
 				title:     "The Mother We Share",
 				timestamp: strconv.FormatInt(time.Now().Unix(), 10)},
-			true},
+		},
 		{"LogLineToTrack",
-			"50 Cent	Get Rich Or Die Tryin'	Many Men (Wish Death)" +
-				"	2	179	L	1579643462",
+			[]string{"50 Cent", "Get Rich Or Die Tryin'", "Many Men (Wish Death)", "2", "179", "L", "1579643462"},
 			ZERO_OFFSET,
 			Track{
 				artist:    "50 Cent",
 				album:     "Get Rich Or Die Tryin'",
 				title:     "Many Men (Wish Death)",
 				timestamp: "1579643462"},
-			true},
+		},
 	}
 
 	for _, test := range testCases {
 		t.Run(test.name, func(t *testing.T) {
-			gotTrack, gotListen := StringToTrack(test.input, test.offset)
+			gotTrack := StringToTrack(test.input, test.offset)
 			trackEqual := true
-			if gotListen == true {
-				if !(test.expectedTrack.artist == gotTrack.artist) {
-					trackEqual = false
-				} else if !(test.expectedTrack.album == gotTrack.album) {
-					trackEqual = false
-				} else if !(test.expectedTrack.title == gotTrack.title) {
-					trackEqual = false
-				} else if !(test.expectedTrack.timestamp ==
-					gotTrack.timestamp) {
-					trackEqual = false
-				}
-
-				if !trackEqual {
-					t.Errorf("Created track was not equal to expected!\n")
-				}
-
-			} else if gotListen != test.expectedListen {
-				t.Errorf("Expected %t got %t\n", test.expectedListen, gotListen)
+			if !(test.expectedTrack.artist == gotTrack.artist) {
+				trackEqual = false
+			} else if !(test.expectedTrack.album == gotTrack.album) {
+				trackEqual = false
+			} else if !(test.expectedTrack.title == gotTrack.title) {
+				trackEqual = false
+			} else if !(test.expectedTrack.timestamp ==
+				gotTrack.timestamp) {
+				trackEqual = false
 			}
+
+			if !trackEqual {
+				t.Errorf("Created track was not equal to expected!\n")
+			}
+
 		})
 	}
 
-} */
+}
 
 func TestConvertTimeStamp(t *testing.T) {
 	testCases := []struct {
